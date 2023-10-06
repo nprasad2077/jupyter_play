577275d9574cc091066c2193550d71f32037caa1
Nikhil Prasad <nprasad2077@gmail.com>
2023-10-05 20:32:26 -0500

    response extracted

--- scripts/openai_python_bug_fix.py
+++ scripts/openai_python_bug_fix.py
@@ -1,27 +1,31 @@
-import os
 import openai
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 
-python_code = input('enter code')
+# python_code = input('enter code: ')
 
 openai.api_key = os.getenv("OPENAI_KEY")
 
 response = openai.ChatCompletion.create(
-  model="gpt-4",
-  messages=[
-    {
-      "role": "system",
-      "content": "You will be provided with a piece of Python code, and your task is to find and fix bugs in it."
-    },
-    {
-      "role": "user",
-      "content": "import Random\na = random.randint(1,12)\nb = random.randint(1,12)\nfor i in range(10):\n    question = \"What is \"+a+\" x \"+b+\"? \"\n    answer = input(question)\n    if answer = a*b\n        print (Well done!)\n    else:\n        print(\"No.\")"
-    }
-  ],
-  temperature=0.52,
-  max_tokens=8000,
-  top_p=1,
-  frequency_penalty=0.75,
-  presence_penalty=0.34,
-  stop=["--stop"]
+    model="gpt-4",
+    messages=[
+        {
+          "role": "system",
+          "content": "You will be provided with a piece of Python code, and your task is to find and fix bugs in it."
+        },
+        {
+            "role": "user",
+            "content": "import Random\na = random.randint(1,12)\nb = random.randint(1,12)\nfor i in range(10):\n    question = \"What is \"+a+\" x \"+b+\"? \"\n    answer = input(question)\n    if answer = a*b\n        print (Well done!)\n    else:\n        print(\"No.\")"
+        }
+    ],
+    temperature=0.52,
+    max_tokens=8000,
+    top_p=1,
+    frequency_penalty=0.75,
+    presence_penalty=0.34,
+    stop=["--stop"]
 )
+
+print(response)
